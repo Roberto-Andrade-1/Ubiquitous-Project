1e8cfe907f40456da1572c5f97c3da287f8da112
Roberto-Andrade-1 <2065421@student.uma.pt>
2023-08-14 20:23:24 +0100

    design updates

--- app/src/main/java/com/example/finalproject/WorkoutPlanExercises.java
+++ app/src/main/java/com/example/finalproject/WorkoutPlanExercises.java
@@ -1,6 +1,9 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,11 +18,14 @@ public class WorkoutPlanExercises extends AppCompatActivity {
     private RecyclerView recyclerViewWorkoutPlanExercise;
     private WorkoutPlanExerciseAdapter adapter;
     private int planID;
+    private Button goBack;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_plan_exercises);
+
+        goBack = findViewById(R.id.backBUTTON);
 
         planID = getIntent().getIntExtra("WORKOUT_PLAN_ID",-1);
 
@@ -28,6 +34,14 @@ public class WorkoutPlanExercises extends AppCompatActivity {
 
         adapter=new WorkoutPlanExerciseAdapter(getExercisesFromWorkoutPlan(planID),this);
         recyclerViewWorkoutPlanExercise.setAdapter(adapter);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WorkoutPlanExercises.this, WorkoutPlanScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private List<ExerciseModel> getExercisesFromWorkoutPlan(int planID){
